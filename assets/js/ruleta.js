08cab224f9ba901c3f571e31f26bedca50253a16
Lapidari0 <wjacobssuazo@gmail.com>
2026-06-12 16:52:42 -0400

    tarea ruleta

--- assets/js/ruleta.js
+++ assets/js/ruleta.js
@@ -26,37 +26,45 @@ const participantes = [
     "Damazo Sepúlveda"
 ];
 
-const colores = [
-    "azul",
-    "rojo",
-    "violeta",
-    "morado"
-]
-
 function lanzarRuleta(min, max) {
-    const numeroAleatorio = Math.random() // entre 0 y <1
+    const numeroAleatorio = Math.random();
 
-    const resultado = Math.floor(numeroAleatorio * (max - min + 1)) + min
+    const resultado = Math.floor(numeroAleatorio * (max - min + 1)) + min;
 
-    return resultado
+    return resultado;
 }
 
 function aleatorioDesdeArreglo(arreglo) {
-    const index = lanzarRuleta(0, arreglo.length - 1)
-    const elementoArreglo = arreglo[index]
+    const index = lanzarRuleta(0, arreglo.length - 1);
+    const elementoArreglo = arreglo[index];
 
-    return elementoArreglo
+    return elementoArreglo;
 }
 
 function renderizarResultado(idElement, texto) {
-    const elementoARenderizar = document.getElementById(idElement)
+    const elementoARenderizar = document.getElementById(idElement);
 
-    elementoARenderizar.textContent = texto
+    elementoARenderizar.textContent = texto;
 }
 
-const getRandomButton = document.querySelector("#getRandom")
+const getRandomButton = document.querySelector("#getRandom");
 
 getRandomButton.addEventListener("click", () => {
-    const resultado = aleatorioDesdeArreglo(participantes)  
-    renderizarResultado("resultado", resultado)
-})
+    if (participantes.length === 0) {
+        renderizarResultado("resultado", "¡Todos participaron!");
+    } else {
+        const resultado = aleatorioDesdeArreglo(participantes);
+
+        renderizarResultado("resultado", resultado);
+
+        const participanteIndex = participantes.indexOf(resultado);
+
+        console.log(participanteIndex + " " + resultado);
+
+        participantes.splice(participanteIndex, 1);
+
+        console.log(participantes.length);
+    }
+});
+
+
