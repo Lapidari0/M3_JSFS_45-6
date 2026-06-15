5109def08c8850322a2b71886d42f28cac80f856
Lapidari0 <wjacobssuazo@gmail.com>
2026-06-15 03:13:31 -0400

    L3. tarea meses y estaciones del año

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -13,11 +13,21 @@ const meses = [
     "Diciembre"
 ]
 
-const mesSeleccionado = prompt("¿En qué mes naciste?, pon un número del 1 al 12")
+const mesSeleccionado = prompt("¿En qué mes naciste? Ingresa un número del 1 al 12")
 const mesSeleccionadoNumber = Number(mesSeleccionado)
 
-if(!mesSeleccionadoNumber || mesSeleccionado <= 0 || mesSeleccionado > 12) {
-    console.log("Porfavor ingresa un número válido")
+if (!mesSeleccionadoNumber || mesSeleccionadoNumber <= 0 || mesSeleccionadoNumber > 12) {
+    console.log("Por favor ingresa un número válido")
 } else {
-    console.log('El mes seleccionado es: ' + meses[mesSeleccionadoNumber - 1])
+    console.log("El mes seleccionado es: " + meses[mesSeleccionadoNumber - 1])
+
+    if (mesSeleccionadoNumber === 12 || mesSeleccionadoNumber === 1 || mesSeleccionadoNumber === 2) {
+        console.log("Naciste en Verano")
+    } else if (mesSeleccionadoNumber >= 3 && mesSeleccionadoNumber <= 5) {
+        console.log("Naciste en Otoño")
+    } else if (mesSeleccionadoNumber >= 6 && mesSeleccionadoNumber <= 8) {
+        console.log("Naciste en Invierno")
+    } else {
+        console.log("Naciste en Primavera")
+    }
 }
